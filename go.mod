f11254159f0f4d578f31edbba6f548c52f8f12bd
muerewa <ffsocietyy@protonmail.com>
2025-07-23 01:32:51 +0300

    added workflow

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/muerewa/grpc-protos
+
+go 1.24.4
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+GITHUB_USERNAME="ffsocietyy@protonmail.com"
+GITHUB_EMAIL="muerewa"
+
+SERVICE_NAME=$1
+RELEASE_VERSION=$2
+
+echo "Generating Go source code"
+mkdir -p golang
+protoc --go_out=./golang \
+  --go_opt=paths=source_relative \
+  --go-grpc_out=./golang \
+  --go-grpc_opt=paths=source_relative \
+ ./${SERVICE_NAME}/*.proto
+
+echo "Generated Go source code files"
+ls -al ./golang/${SERVICE_NAME}
+
+cd golang/${SERVICE_NAME}
+go mod init \
+  github.com/${GITHUB_USERNAME}/grpc-protos/golang/${SERVICE_NAME} || true
+go mod tidy || true
+cd ../../
+git config --global user.email ${GITHUB_EMAIL}
+git config --global user.name ${GITHUB_USERNAME}
+git add . && git commit -am "proto update" || true
+git push -u origin HEAD
+git tag -d golang/${SERVICE_NAME}/${RELEASE_VERSION}
+git push --delete origin golang/${SERVICE_NAME}/${RELEASE_VERSION}
+git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
+  -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
